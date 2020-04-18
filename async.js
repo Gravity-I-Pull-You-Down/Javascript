40a0e5ab2ad0f265e0f4481594029d970989a2b8
Viveksharma48b <viveksharma48b@gmail.com>
2020-04-18 14:18:59 +0530

    first js commit

---
+++ async.js
@@ -0,0 +1,12 @@
+function one(){
+console.log("one")
+}
+function Two(){
+console.log("Two")
+}
+function Three(){
+console.log("Three")
+}
+setTimeout(one,1000);
+setTimeout(Two ,0 );
+Three();
---
+++ first.js
@@ -0,0 +1,7 @@
+const o = {sno:1,
+FirstName:"Vivek",
+LastName:"Sharma",
+Age:22,
+Occupation:"Student"
+}
+console.log(o.FirstName);
